6b1a8c8553eda399fae270ada1150963d710da78
Guilherme Artigas <Guilherme.Artigas92@gmail.com>
2022-12-03 15:24:01 -0300

    docs: requisito 06*

---
+++ desafio6.sql
@@ -0,0 +1,8 @@
+SELECT
+MIN(P.valor) AS 'faturamento_minimo',
+MAX(P.valor) AS 'faturamento_maximo',
+ROUND(AVG(P.valor), 2) AS 'faturamento_medio',
+SUM(P.valor) AS 'faturamento_total'
+FROM SpotifyClone.usuarios AS U
+INNER JOIN SpotifyClone.planos AS P
+ON U.plano = P.plano_id;
